5c247761f79972f40633d2b77e731318a1da698a
Wolvorine871 <49442701+Wolvorine871@users.noreply.github.com>
2019-04-13 21:49:55 +0530

    Add files via upload

---
+++ attemp1.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+int students[3][5],ns=3,scheduler[1000],arrivedStudents[100],nap=0;
+unsigned int time=0;
+
+void getSystem()
+{
+		printf("\n Arrival Time of P(A) having PID:-%d-> ",2132);
+		scanf("%d",&students[0][0]);
+		printf("\n Burst Time of P(A) having PID:-%d-> ",2132);
+		scanf("%d",&students[0][1]);
+		printf("\n Arrival Time of P(B) having PID:-%d-> ",2102);
+		scanf("%d",&students[1][0]);
+		printf("\n Burst Time of P(B) having PID:-%d-> ",2102);
+		scanf("%d",&students[1][1]);
+		printf("\n Arrival Time of P(C) having PID:-%d-> ",2453);
+		scanf("%d",&students[2][0]);
+		printf("\n Burst Time of P(C) having PID:-%d-> ",2453);
+		scanf("%d",&students[2][1]);
+		for(int i=0;i<ns;i++)
+		students[i][2]=students[i][1];
+    	printf("\n ----------------------------");	
+
+}
+
