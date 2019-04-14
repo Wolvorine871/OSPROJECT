362a2a00715f4afdf90d5a67bb7ca1f97b284a2b
Wolvorine871 <49442701+Wolvorine871@users.noreply.github.com>
2019-04-14 20:58:54 +0530

    Add files via upload

---
+++ attemp5.cpp
@@ -0,0 +1,136 @@
+#include<stdio.h>
+#include<stdlib.h>
+int students[3][5],ns=3,scheduler[1000],arrivedStudents[100],nap=0;
+unsigned int time=0;
+
+void getSystem()
+{
+		printf("\n Arrival Time of P(A) having PID:-%d-> ",2132);
+		scanf("%d",&students[0][0]);
+		printf("\n Burst Time of P(A) having PID:-%d-> ",2132);
+		scanf("%d",&students[0][1]);
+		printf("\n Arrival Time of P(B) having PID:-%d-> ",2102);
+		scanf("%d",&students[1][0]);
+		printf("\n Burst Time of P(B) having PID:-%d-> ",2102);
+		scanf("%d",&students[1][1]);
+		printf("\n Arrival Time of P(C) having PID:-%d-> ",2453);
+		scanf("%d",&students[2][0]);
+		printf("\n Burst Time of P(C) having PID:-%d-> ",2453);
+		scanf("%d",&students[2][1]);
+		for(int i=0;i<ns;i++)
+		students[i][2]=students[i][1];
+    	printf("\n ----------------------------");	
+
+}
+
+void printSystem()
+{
+	printf("\n\t\tOur System is :");
+	printf("\nSi:  AT  BT RT");
+	for(int i=0; i<ns; i++)
+	{
+		printf("\nS%d:  %d  %d  %d", i+1, students[i][0], students[i][1], students[i][2]);
+	}
+	printf("\n ---------------------------------");
+} 
+
+unsigned int remainingTime()
+{
+	for(int i=0;i<ns;i++)
+	{
+		if(students[i][2]>0)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void addArivedStudents()
+{
+	for(int i=0;i<ns;i++)
+	{
+		if(students[i][0]==time)
+		{
+			arrivedStudents[nap]=i;
+			nap++;
+		}
+	}
+}
+
+void printArrivedStudents()
+{
+	printf("\n\nArrived Students %d : ",nap);   //nap=no of arrived students
+	for(int i=0;i<nap;i++)
+	{
+		printf(" %d ",arrivedStudents[i]+1);
+	}
+}
+
+int getLRT()
+{
+	if(nap==0)
+	{
+		return -1;
+	}
+	unsigned int largestRTP,largestRTPIndex;
+	largestRTPIndex=arrivedStudents[0];
+	largestRTP=students[arrivedStudents[0]][2];
+	for(int i=0;i<nap;i++)
+	{
+		if(students[arrivedStudents[i]][2]>largestRTP)
+		{
+			largestRTP = students[arrivedStudents[i]][2];
+			largestRTPIndex = arrivedStudents[i];
+		}
+	}
+	return largestRTPIndex;
+}
+
+void removeFromArrivedProcesses(int toRun)
+{
+	printArrivedStudents();
+	int temp[100];
+	int x = 0;
+	for(int i=0; i< nap; i++)
+	{
+		if(arrivedStudents[i] != toRun)
+		{
+			temp[x] = arrivedStudents[i];
+			x++;
+		}
+	}
+	nap=nap-1;
+	for(int i=0; i<nap; i++)
+	{
+		arrivedStudents[i] = temp[i];
+	}	
+}
+
+void schedule()
+{
+	int toRun;
+	while(remainingTime()) //boolean function
+	{
+		addArivedStudents();
+		printArrivedStudents();
+		toRun = getLRT();
+		scheduler[time] = toRun;
+		students[toRun][2]=students[toRun][2]-1;
+		if(students[toRun][2]==0)
+		{
+			removeFromArrivedProcesses(toRun);
+		}
+		if(toRun==-1)
+		{
+		printf("\nRun=NULL, Time=%d",time);
+		}
+		else
+		{
+		printf("\nRun=S%d,Time = %d",toRun+1,time);
+		}
+		printSystem();
+		time++;
+	}
+}
+
